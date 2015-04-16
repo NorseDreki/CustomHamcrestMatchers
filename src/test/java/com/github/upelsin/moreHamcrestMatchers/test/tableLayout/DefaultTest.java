24d14c56204b979020ba9a2a83f15f9247495031
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2015-04-17 01:58:00 +0400

    Added a Robolectric test.

---
+++ src/test/java/com/github/upelsin/moreHamcrestMatchers/test/tableLayout/DefaultTest.java
@@ -0,0 +1,15 @@
+package com.github.upelsin.moreHamcrestMatchers.test.tableLayout;
+
+import org.junit.Test;
+import org.robolectric.Robolectric;
+
+/**
+ * Created by upelsin on 16.04.2015.
+ */
+public class DefaultTest {
+
+    @Test
+    public void should() {
+        //Robolectric.buildActivity()
+    }
+}
